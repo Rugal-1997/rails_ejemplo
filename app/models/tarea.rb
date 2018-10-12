2bf8f6fdb3e71f94731cde2b36937b09f807b607
Rugal-1997 <43483646+Rugal-1997@users.noreply.github.com>
2018-10-12 13:27:32 -0500

    Update tarea.rb

--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -1,6 +1,7 @@
 class Tarea < ApplicationRecord
 	belongs_to :usuario
 	has_many :comentarios
+	has_many :comentarios, dependent: :destroy
 	validates :titulo, presence: true
 	validates :descripcion, presence: true
 end
